80a38d0cc26d3a3816e9f8f04c1d94de951a087a
Weibo Hsu <hsuweibo@yahoo.com>
2020-10-01 02:57:04 -0400

    Updates

---
+++ precache-manifest.8da0234c825977197480dced25d97a2a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8602d271611b5101843f2862b7bda210",
+    "url": "/Covid19Tracker/index.html"
+  },
+  {
+    "revision": "2a5c623b9ebb56f522cf",
+    "url": "/Covid19Tracker/static/css/main.6507cde7.chunk.css"
+  },
+  {
+    "revision": "6d5559c7aae27c7da9a8",
+    "url": "/Covid19Tracker/static/js/2.5eff8318.chunk.js"
+  },
+  {
+    "revision": "5687e21765225d8eb60c5ad560a94200",
+    "url": "/Covid19Tracker/static/js/2.5eff8318.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a5c623b9ebb56f522cf",
+    "url": "/Covid19Tracker/static/js/main.bd70f4a5.chunk.js"
+  },
+  {
+    "revision": "d08f0b21a31ad4228eb0",
+    "url": "/Covid19Tracker/static/js/runtime-main.c6fbbc6e.js"
+  },
+  {
+    "revision": "1a59560a4208fb602b18bb412c8ab716",
+    "url": "/Covid19Tracker/static/media/globe.1a59560a.jpg"
+  }
+]);
